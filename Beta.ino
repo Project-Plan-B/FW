9b14a686ac311846abc8abfc8bd9fa6c06a344fb
Yu Jun Choi <dmddkslek0@gmail.com>
2024-07-11 22:27:20 +0900

    ⚡️
    복붙 잘못해서 커밋 잘못했습니당 ㅎㅎ... 다시 올립니다.

--- Beta.ino
+++ Beta.ino
@@ -1,5 +1,6 @@
 #include <DHT.h>
 #include <SoftwareSerial.h>
+#include <ArduinoJson.h>
 
 #define DHTPIN 2
 #define DHTTYPE DHT22
@@ -34,17 +35,34 @@ void loop() {
   Serial.print("CO2: ");
   Serial.println(co2Value);
 
-  String data = "temperature=" + String(t) + "&humidity=" + String(h) + "&co2=" + String(co2Value);
-  sendToServer(data);
+  String jsonData = createJSONData(t, h, co2Value);
+  sendToServer(jsonData);
 
   delay(2000);
 }
 
-void sendToServer(String data) {
-  espSerial.println("AT+CIPSTART=\"TCP\",\"<Node.js 서버 IP>\",<포트>");
+String createJSONData(float temperature, float humidity, int co2) {
+  StaticJsonDocument<200> doc;
+  doc["temperature"] = temperature;
+  doc["humidity"] = humidity;
+  doc["co2"] = co2;
+
+  String jsonData;
+  serializeJson(doc, jsonData);
+  return jsonData;
+}
+
+void sendToServer(String jsonData) {
+  espSerial.println("AT+CIPSTART=\"TCP\",\"<서버 IP>\",<포트>");
   delay(2000);
 
-  String httpRequest = "GET /update?" + data + " HTTP/1.1\r\nHost: <Node.js 서버 IP>\r\nConnection: close\r\n\r\n";
+  String httpRequest = "POST /update HTTP/1.1\r\n";
+  httpRequest += "Host: <서버 IP>\r\n";
+  httpRequest += "Content-Type: application/json\r\n";
+  httpRequest += "Content-Length: " + String(jsonData.length()) + "\r\n";
+  httpRequest += "Connection: close\r\n\r\n";
+  httpRequest += jsonData;
+
   espSerial.print("AT+CIPSEND=");
   espSerial.println(httpRequest.length());
   delay(1000);
