59876ecb6f86b792e17cc0953043ee32f50a1251
Yu Jun Choi <dmddkslek0@gmail.com>
2024-08-01 21:48:09 +0900

    🐛
    Server 연동 실패 오류 수정 및 loop 오류 수정

--- Beta.ino
+++ Beta.ino
@@ -7,12 +7,12 @@ DHTesp dht;
 const char* ssid = "SSID";
 const char* password = "PW";
 const char* server = "IP";
-const int port = 1234;
+const int port = 1234; // '1234' is test number.
 
 void setup() {
   Serial.begin(115200);
   Serial.println();
-  Serial.println("Status\tHumidity (%)\tTemperature (C)\t(F)\tHeatIndex (C)\t(F)\tCO2");
+  Serial.println("Status\tHumidity (%)\tTemperature (C)\t(F)\tHeatIndex (C)\t(F)");
 
   dht.setup(DHTPIN, DHTesp::DHT22);
 
@@ -29,7 +29,6 @@ void loop() {
 
   float humidity = dht.getHumidity();
   float temperature = dht.getTemperature();
-  int co2 = analogRead(A0);
 
   if (isnan(humidity) || isnan(temperature)) {
     Serial.println("Failed to read from DHT sensor!");
@@ -46,23 +45,22 @@ void loop() {
   Serial.print("\t\t");
   Serial.print(dht.computeHeatIndex(temperature, humidity, false), 1);
   Serial.print("\t\t");
-  Serial.print(dht.computeHeatIndex(dht.toFahrenheit(temperature), humidity, true), 1);
-  Serial.print("\t\t");
-  Serial.println(co2);
+  Serial.println(dht.computeHeatIndex(dht.toFahrenheit(temperature), humidity, true), 1);
 
-  String jsonPayload = "{\"temperature\":" + String(temperature) + ",\"humidity\":" + String(humidity) + ",\"co2\":" + String(co2) + "}";
+  String jsonPayload = "{\"temperature\":" + String(temperature) + ",\"humidity\":" + String(humidity) + "}";
   Serial.println("Sending data: " + jsonPayload);
 
   if (WiFi.status() == WL_CONNECTED) {
     WiFiClient client;
     if (client.connect(server, port)) {
-      client.println("POST : Port");
+      client.println("POST /info/other HTTP/1.1");
       client.println("Host: " + String(server));
       client.println("Content-Type: application/json");
       client.print("Content-Length: ");
       client.println(jsonPayload.length());
       client.println();
       client.println(jsonPayload);
+      client.stop();
     } else {
       Serial.println("Failed to connect to server");
     }
